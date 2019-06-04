ba84c6dc9842a8bf3fa76c78877b27b79a41d8b5
Roberto Rodríguez Echeverría <rrecheve@gmail.com>
2019-06-04 12:43:30 +0200

    fixed tool bar issue & GameDescription started from list

--- app/src/main/java/es/unex/saee/sonicmusiccollection/GameDescription.java
+++ app/src/main/java/es/unex/saee/sonicmusiccollection/GameDescription.java
@@ -1,7 +1,10 @@
 package es.unex.saee.sonicmusiccollection;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
 
 public class GameDescription extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class GameDescription extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_description);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("GAME_MESSAGE");
+
+        TextView title = (TextView) findViewById(R.id.textView7);
+        title.setText(message);
     }
 }
